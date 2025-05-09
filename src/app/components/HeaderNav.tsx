e2e427e3d3b2585020ff42bcdd8a638a060a2c4f
Quin Carter <quin.carter@gmail.com>
2025-05-08 21:35:49 -0400

    added icons

--- src/app/components/HeaderNav.tsx
+++ src/app/components/HeaderNav.tsx
@@ -92,7 +92,23 @@ export default function HeaderNav({ session }: { session: any }) {
                   {session.user?.email}
                 </li>
                 <li>
-                  <Link href="/profile">Profile</Link>
+                  <Link href="/profile" className="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 mr-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                    Profile
+                  </Link>
                 </li>
                 {/* Settings link with icon - visible on all screens */}
                 <li>
