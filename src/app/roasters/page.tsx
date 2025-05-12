9fb8eaaaab4d2b12cd8bb9374ee7bf1f84b7e981
Quin Carter <quin.carter@gmail.com>
2025-05-11 22:46:27 -0400

    Added coffee roaster blurb alert

--- src/app/roasters/page.tsx
+++ src/app/roasters/page.tsx
@@ -134,6 +134,30 @@ export default function RoastersPage() {
 
   return (
     <>
+      <div
+        style={{ marginInline: "auto" }}
+        className={`flex alert alert-neutral text-sm mb-4 max-w-3xl self-center`}
+      >
+        <p>
+          Coffee Roasters and Coffees are now separate entities. You can add a
+          new roaster and then add coffees to it. They are also shared with the
+          entire BrewMe community for use in their brew profiles. <br /> <br />
+          Right now it is very tedious to add roasters and coffees, but this is
+          how the community will grow and thrive!
+          <br />
+          <br /> If you have any questions or feedback, please reach out to us
+          on{" "}
+          <a
+            href="https://github.com/quincarter/coffee-app/issues"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary hover:underline"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
       <FilterableList
         title="Coffee Roasters"
         items={roasters}
